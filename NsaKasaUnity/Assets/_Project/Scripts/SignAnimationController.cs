cff949b2bec4b77e546097c85e12d1e5e91649fc
Elliot Datsomor <datsomorelliot56@gmail.com>
2026-05-08 09:17:46 +0000

    Add meta file for recovery asset in Unity project

--- NsaKasaUnity/Assets/_Project/Scripts/SignAnimationController.cs
+++ NsaKasaUnity/Assets/_Project/Scripts/SignAnimationController.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class SignAnimationController : MonoBehaviour
 {
+    [SerializeField] private Animator avatarAnimator;
+
     public void PlayThankYou()
     {
-        Debug.Log("THANK_YOU button clicked.");
+        Debug.Log("THANK_YOU animation requested.");
+
+        if (avatarAnimator == null)
+        {
+            Debug.LogWarning("Avatar Animator is not assigned.");
+            return;
+        }
+
+        avatarAnimator.Play("THANK_YOU_TEST", 0, 0f);
     }
 }
